4f36312713d565c19ffe63bc1fd62368e002804b
Lakshya Kaul <lakshyakaul05@gmail.com>
2025-09-02 19:46:44 +0530

    changed from localhost to url for complete hosting

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- CONFIGURATION ---
-    const API_URL = 'http://localhost:3000/api'; // Replace with your deployed URL if needed
+    const API_URL = 'https://profile-api-fpjk.onrender.com/'; // Replace with your deployed URL if needed
     let API_KEY = null; // We will prompt for this when needed
 
     // --- DOM ELEMENT REFERENCES ---
